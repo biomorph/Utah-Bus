7bd5cf773b9695566c7b2b05037eb9ce5aaa2694
biomorph <ravi@server.com>
2012-08-03 15:02:19 -0600

    Initial Commit

---
+++ Utah Bus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Utah Bus
+//
+//  Created by Ravi Alla on 8/3/12.
+//  Copyright (c) 2012 Ravi Alla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Utah Bus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Utah Bus
+//
+//  Created by Ravi Alla on 8/3/12.
+//  Copyright (c) 2012 Ravi Alla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
